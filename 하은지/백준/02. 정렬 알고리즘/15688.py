0359b66464a754ecc41aa91e0a18b2811ea7e7b7
Eunbyul <89649741+EunByu1@users.noreply.github.com>
2022-05-27 10:03:01 +0900

    [실버 5] 수 정렬하기 5
    https://www.acmicpc.net/problem/15688

--- 하은지/백준/02. 정렬 알고리즘/15688.py
+++ 하은지/백준/02. 정렬 알고리즘/15688.py
@@ -4,6 +4,6 @@ T    = int(sys.stdin.readline())
 list = [int(sys.stdin.readline()) for _ in range(T)]
 
 # [1] 정렬 및 출력
-list.sort(reverse = False)    # defalut value : reverse = True
+list.sort(reverse = False)    
 for i in list:
     print(i)
