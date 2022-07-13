46a480df1809bf40b66e09881235099fa8fcecdd
Yuhee Kim <dbgml4509@naver.com>
2022-07-14 00:12:29 +0900

    [실버 4] 수 찾기
    https://www.acmicpc.net/problem/1920

--- 김유희/백준/1920.py
+++ 김유희/백준/1920.py
@@ -1,28 +1,30 @@
 import sys
+
 input = sys.stdin.readline
 
 N = int(input())
-NList = sorted(map(int,stdin.readline().split()))
+NList = sorted(map(int, input().split()))
 M = int(input())
-MList = sorted(map(int,stdin.readline().split()))
+MList = list(map(int, input().split()))
+
 
 def binarySearch(target):
     left = 0
-    right = n - 1
+    right = N - 1
 
     while left <= right:
         mid = (left + right) // 2
-        if n_list[mid] == target:
+        if NList[mid] == target:
             return True
 
-        if target < n_list[mid]:
-            right = mid-1
-        elif target > n_list[mid]:
+        if target < NList[mid]:
+            right = mid - 1
+        elif target > NList[mid]:
             left = mid + 1
-            
+
+
 for i in range(M):
-    if binary(MList[i]):
+    if binarySearch(MList[i]):
         print(1)
     else:
         print(0)
-    
