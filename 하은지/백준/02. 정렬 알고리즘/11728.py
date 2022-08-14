2bc90a03e631c9cbacaf14d3bd3bae5734126736
Eunbyul <89649741+EunByu1@users.noreply.github.com>
2022-08-14 18:42:37 +0900

    [실버 5] 배열 합치기
    https://www.acmicpc.net/problem/11728

--- 하은지/백준/02. 정렬 알고리즘/11728.py
+++ 하은지/백준/02. 정렬 알고리즘/11728.py
@@ -1,8 +1,11 @@
 # [방법 1] 
 # ---------------------------------------------------------------------
+# [0] 모듈 import
 import sys
 import itertools
 
+
+# [1] 입력 
 N, M     = map(int, sys.stdin.readline().split())
 A        = []
 B        = []
@@ -14,13 +17,20 @@ A.append(list(map(int, sys.stdin.readline().split())))
 B.append(list(map(int, sys.stdin.readline().split())))
 
 
+# [2] 배열 A & B 합친 후 Result_1에 Binding
 Result_1 = A + B
 
+
+# [3] 2차원 형태의 리스트 요소 값을 1차원 형태로 변환 후 Result_2에 Binding
 for i in Result_1:
     Result_2.extend(i)
 
+    
+# [4] 정렬 
 Result_2.sort()
 
+
+# [5] 출력 
 for i in Result_2:
     print(i, end=" ")
 # ---------------------------------------------------------------------
@@ -29,16 +39,26 @@ for i in Result_2:
 
 # [방법 2] 
 # ---------------------------------------------------------------------
+# [0] 모듈 import 
 import sys
 import itertools
 
+
+# [1] 입력 
 N, M  = map(int, sys.stdin.readline().split())
 A     = list(map(int, sys.stdin.readline().split()))
 B     = list(map(int, sys.stdin.readline().split()))
 
+
+# [2] 배열 A & B 합친 후 Result에 Binding
 Result = A + B
+
+
+# [3] 정렬 
 Result.sort()
 
+
+# [4] 출력 
 for i in Result:
     print(i, end=" ")   
 # ---------------------------------------------------------------------
